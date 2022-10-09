2a9510d024f8553267586c7bc92ca9fba1f2fb3d
Povilas Balciunas <balciunas90@gmail.com>
2022-10-09 14:18:59 +0200

    Prep for function building

--- wabbit/wabbit/_compiler.py
+++ wabbit/wabbit/_compiler.py
@@ -25,8 +25,10 @@ class Compiler(Visitor):
         self._main_func = ir.Function(
             self._mod, ir.FunctionType(_TInt, []), name="main"
         )
-        block = self._main_func.append_basic_block("entry")
-        self._ir_builder = ir.IRBuilder(block)
+        block = self._main_func.append_basic_block("main")
+        # IR builder for the `main` function block.
+        self._main_builder = ir.IRBuilder(block)
+
         self._print_int = ir.Function(
             self._mod, ir.FunctionType(_TVoid, [_TInt]), name="__wabbit_print_int"
         )
@@ -41,9 +43,11 @@ class Compiler(Visitor):
         )
 
         self._blocks_nr = 0
+        # When building a function, we will use a different IR builder.
+        self._curr_builder = self._main_builder
 
     def to_llvm(self) -> str:
-        self._ir_builder.ret(ir.Constant(_TInt, 0))
+        self._main_builder.ret(ir.Constant(_TInt, 0))
         return str(self._mod)
 
     def visit_Integer(self, node: Integer) -> ir.Value:
@@ -61,41 +65,41 @@ class Compiler(Visitor):
     def visit_PrintStatement(self, node: PrintStatement) -> None:
         res = self.visit(node.value)
         if res.type == _TInt:
-            self._ir_builder.call(self._print_int, [res])
+            self._curr_builder.call(self._print_int, [res])
         elif res.type == _TFloat:
-            self._ir_builder.call(self._print_float, [res])
+            self._curr_builder.call(self._print_float, [res])
         elif res.type == _TBool:
-            self._ir_builder.call(self._print_bool, [res])
+            self._curr_builder.call(self._print_bool, [res])
         elif res.type == _TChar:
-            self._ir_builder.call(self._print_char, [res])
+            self._curr_builder.call(self._print_char, [res])
 
     def visit_VarDecl(self, node: VarDecl) -> None:
         var_name = node.name.value
         if node.value:
             val = self.visit(node.value)
             if val.type == _TInt:
-                var = self._ir_builder.alloca(_TInt, name=var_name)
+                var = self._curr_builder.alloca(_TInt, name=var_name)
             elif val.type == _TFloat:
-                var = self._ir_builder.alloca(_TFloat, name=var_name)
+                var = self._curr_builder.alloca(_TFloat, name=var_name)
             elif val.type == _TChar:
-                var = self._ir_builder.alloca(_TChar, name=var_name)
+                var = self._curr_builder.alloca(_TChar, name=var_name)
             else:
                 assert False
         else:
             val = _default_var_type(node)
             match node.type_:
                 case Type(name="int"):
-                    var = self._ir_builder.alloca(_TInt, name=var_name)
+                    var = self._curr_builder.alloca(_TInt, name=var_name)
                 case Type(name="float"):
-                    var = self._ir_builder.alloca(_TFloat, name=var_name)
+                    var = self._curr_builder.alloca(_TFloat, name=var_name)
                 case Type(name="char"):
-                    var = self._ir_builder.alloca(_TBool, name=var_name)
+                    var = self._curr_builder.alloca(_TBool, name=var_name)
                 case Type(name="bool"):
-                    var = self._ir_builder.alloca(_TBool, name=var_name)
+                    var = self._curr_builder.alloca(_TBool, name=var_name)
                 case _:
                     assert False
 
-        self._ir_builder.store(val, var)
+        self._curr_builder.store(val, var)
         self._variables[var_name] = var
 
     def visit_ConstDecl(self, node: ConstDecl) -> None:
@@ -103,19 +107,19 @@ class Compiler(Visitor):
         val = self.visit(node.value)
 
         if val.type == _TInt:
-            var = self._ir_builder.alloca(_TInt, name=var_name)
+            var = self._curr_builder.alloca(_TInt, name=var_name)
         elif val.type == _TFloat:
-            var = self._ir_builder.alloca(_TFloat, name=var_name)
+            var = self._curr_builder.alloca(_TFloat, name=var_name)
         elif val.type == _TChar:
-            var = self._ir_builder.alloca(_TChar, name=var_name)
+            var = self._curr_builder.alloca(_TChar, name=var_name)
         else:
             assert False
 
-        self._ir_builder.store(val, var)
+        self._curr_builder.store(val, var)
         self._variables[var_name] = var
 
     def visit_Name(self, node: Name):
-        return self._ir_builder.load(self._variables[node.value])
+        return self._curr_builder.load(self._variables[node.value])
 
     def visit_BinOp(self, node: BinOp) -> t.Any:
         left = self.visit(node.left)
@@ -124,40 +128,40 @@ class Compiler(Visitor):
         match node.operation:
             case "+":
                 if left.type == _TFloat:
-                    return self._ir_builder.fadd(left, right)
+                    return self._curr_builder.fadd(left, right)
                 else:
-                    return self._ir_builder.add(left, right)
+                    return self._curr_builder.add(left, right)
             case "-":
                 if left.type == _TFloat:
-                    return self._ir_builder.fsub(left, right)
+                    return self._curr_builder.fsub(left, right)
                 else:
-                    return self._ir_builder.sub(left, right)
+                    return self._curr_builder.sub(left, right)
             case "*":
                 if left.type == _TFloat:
-                    return self._ir_builder.fmul(left, right)
+                    return self._curr_builder.fmul(left, right)
                 else:
-                    return self._ir_builder.mul(left, right)
+                    return self._curr_builder.mul(left, right)
             case "/":
                 if left.type == _TFloat:
-                    return self._ir_builder.fdiv(left, right)
+                    return self._curr_builder.fdiv(left, right)
                 else:
-                    return self._ir_builder.sdiv(left, right)
+                    return self._curr_builder.sdiv(left, right)
 
     def visit_UnaryOp(self, node: UnaryOp) -> t.Any:
         match node.operation:
             case "-":
                 val = self.visit(node.operand)
                 if val.type == _TInt:
-                    return self._ir_builder.neg(val)
+                    return self._curr_builder.neg(val)
                 else:
-                    return self._ir_builder.fneg(val)
+                    return self._curr_builder.fneg(val)
 
             case "+":
                 return self.visit(node.operand)
 
             case "!":
                 val = self.visit(node.operand)
-                return self._ir_builder.not_(val)
+                return self._curr_builder.not_(val)
 
     def visit_LogicalOp(self, node: LogicalOp) -> t.Any:
         left = self.visit(node.left)
@@ -165,38 +169,38 @@ class Compiler(Visitor):
         match node.operation:
             case "==":
                 if left.type == _TFloat:
-                    return self._ir_builder.fcmp_ordered("==", left, right)
+                    return self._curr_builder.fcmp_ordered("==", left, right)
                 else:
-                    return self._ir_builder.icmp_signed("==", left, right)
+                    return self._curr_builder.icmp_signed("==", left, right)
             case "!=":
                 if left.type == _TFloat:
-                    return self._ir_builder.fcmp_ordered("!=", left, right)
+                    return self._curr_builder.fcmp_ordered("!=", left, right)
                 else:
-                    return self._ir_builder.icmp_signed("!=", left, right)
+                    return self._curr_builder.icmp_signed("!=", left, right)
             case ">":
                 if left.type == _TFloat:
-                    return self._ir_builder.fcmp_ordered(">", left, right)
+                    return self._curr_builder.fcmp_ordered(">", left, right)
                 else:
-                    return self._ir_builder.icmp_signed(">", left, right)
+                    return self._curr_builder.icmp_signed(">", left, right)
             case ">=":
                 if left.type == _TFloat:
-                    return self._ir_builder.fcmp_ordered(">=", left, right)
+                    return self._curr_builder.fcmp_ordered(">=", left, right)
                 else:
-                    return self._ir_builder.icmp_signed(">=", left, right)
+                    return self._curr_builder.icmp_signed(">=", left, right)
             case "<":
                 if left.type == _TFloat:
-                    return self._ir_builder.fcmp_ordered("<", left, right)
+                    return self._curr_builder.fcmp_ordered("<", left, right)
                 else:
-                    return self._ir_builder.icmp_signed("<", left, right)
+                    return self._curr_builder.icmp_signed("<", left, right)
             case "<=":
                 if left.type == _TFloat:
-                    return self._ir_builder.fcmp_ordered("<=", left, right)
+                    return self._curr_builder.fcmp_ordered("<=", left, right)
                 else:
-                    return self._ir_builder.icmp_signed("<=", left, right)
+                    return self._curr_builder.icmp_signed("<=", left, right)
             case "&&":
-                return self._ir_builder.and_(left, right)
+                return self._curr_builder.and_(left, right)
             case "||":
-                return self._ir_builder.or_(left, right)
+                return self._curr_builder.or_(left, right)
             case _:
                 raise RuntimeError("not possible")
 
@@ -206,7 +210,7 @@ class Compiler(Visitor):
 
         value = self.visit(node.right)
         var = self._variables[var_name]
-        self._ir_builder.store(value, var)
+        self._curr_builder.store(value, var)
 
         return value
 
@@ -218,18 +222,18 @@ class Compiler(Visitor):
         else_block = self._next_block(self._main_func) if node.else_body else None
         merge_block = self._next_block(self._main_func)
 
-        self._ir_builder.cbranch(test_res, then_block, else_block or merge_block)
+        self._curr_builder.cbranch(test_res, then_block, else_block or merge_block)
 
-        self._ir_builder.position_at_end(then_block)
+        self._curr_builder.position_at_end(then_block)
         self.visit(node.body)
-        self._ir_builder.branch(merge_block)
+        self._curr_builder.branch(merge_block)
 
         if else_body := node.else_body:
-            self._ir_builder.position_at_end(else_block)
+            self._curr_builder.position_at_end(else_block)
             self.visit(else_body)
-            self._ir_builder.branch(merge_block)
+            self._curr_builder.branch(merge_block)
 
-        self._ir_builder.position_at_end(merge_block)
+        self._curr_builder.position_at_end(merge_block)
 
     def visit_While(self, node: While) -> t.Any:
         """
@@ -251,16 +255,16 @@ class Compiler(Visitor):
         loop_test_block = self._next_block(self._main_func)
 
         # Seems like we always need to branch into the label.
-        self._ir_builder.branch(loop_test_block)
-        self._ir_builder.position_at_end(loop_test_block)
+        self._curr_builder.branch(loop_test_block)
+        self._curr_builder.position_at_end(loop_test_block)
         test_res = self.visit(node.test)
-        self._ir_builder.cbranch(test_res, loop_body_block, loop_exit_block)
+        self._curr_builder.cbranch(test_res, loop_body_block, loop_exit_block)
 
-        self._ir_builder.position_at_end(loop_body_block)
+        self._curr_builder.position_at_end(loop_body_block)
         self.visit(node.body)
-        self._ir_builder.branch(loop_test_block)
+        self._curr_builder.branch(loop_test_block)
 
-        self._ir_builder.position_at_end(loop_exit_block)
+        self._curr_builder.position_at_end(loop_exit_block)
 
     def visit_Statements(self, node: Statements) -> None:
         for n in node.nodes:
